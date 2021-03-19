a03ce6442a4f3400ee7d319e0bd1bcd2a8798313
Cédric Jean <cedric.jean@momentum-tech.ca>
2021-03-19 11:00:32 -0400

    Eslintrc to the good place

--- src/components/AzureButton/index.js
+++ src/components/AzureButton/index.js
@@ -1,10 +1,13 @@
 import azureFunction from "./azureFunction"
 import {Button} from "@material-ui/core"
 const buttonAzure= ()=>{
+    const buttononclick = () =>{
+        azureFunction()
+    }
     return(
         <div class="border text-justify">
             <div>Ceci est la composante 3</div>
-            <Button class="btn btn-success"onClick={azureFunction}>Azure Button</Button>
+            <Button class="btn btn-success"onClick={buttononclick}>Azure Button</Button>
         </div>
     )
 }
